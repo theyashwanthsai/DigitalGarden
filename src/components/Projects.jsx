ff4fbf2e874a13dc95d672eafd9dc27c12dda737
Sai Yashwanth <saiyashwanth@Sais-MacBook-Air.local>
2023-05-29 23:37:13 +0530

    make changes to project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,21 +5,21 @@ import React from "react"
 function Projects() {
   return(
     <div className = "p-5 flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className = "p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Projects</h1>
-      <p className="p-10 text-lg max-w-xl mb-6 font-bold">
+      <h1 className = "p-2 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Projects</h1>
+      <p className="p-2 text-lg max-w-xl mb-6 font-bold">
         I always believe in learning by building. I love building - a product, experiment, what ever. 
         Building is what I wanted to do since my childhood. Turn your thoughts into something real, something which exists. 
         Projects are what I have been building from the past two years. Here are few of the best projects I built. </p>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
-      <ul className="p-3">hi</ul>
+      <ul className="p-3">UltraSonicFPS</ul>
+      <ul className="p-3">Raycasting 3d Engine</ul>
+      <ul className="p-3">ActionRPG</ul>
+      <ul className="p-3">Anime Recommendation System</ul>
+      <ul className="p-3">Toy Chess Engine</ul>
+      <ul className="p-3">Theft Detection System</ul>
+      <ul className="p-3">First-Person-Shooter</ul>
+      <ul className="p-3">Snake Game</ul>
+      <ul className="p-3">CR7 Analysis</ul>
+      <ul className="p-3">CryptoTwitterBOT</ul>
     </div>
   );
 
