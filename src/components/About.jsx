a6d15121921fdb14761411f1ac87d298f6245985
Sai Yashwanth <saiyashwanth@Sais-MacBook-Air.local>
2023-05-29 20:25:24 +0530

    contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return(
     <div className = "p-5 flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className = "p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">About</h1>
-      <p>Hello, I am Sai Yashwanth, a Computer Science Engineer from Bangalore currently pursuing my undergrad from PES University. I am a Developer interested in Backend development, Devops and Machine Learning. I love building products which help others in some way or the other. I believe in learning in public, That is how I make friends and in-turn learn from them and their experiences. <br /> You can check what I'm building <a className= "underline" href="https://github.com/theyashwanthsai">here</a> . </p>
+      <p className="p-10 text-lg max-w-xl mb-6 font-bold">Hello, I am Sai Yashwanth, a Computer Science Engineer from Bangalore currently pursuing my undergrad from PES University. I am a Developer interested in Backend development, Devops and Machine Learning. I love building products which help others in some way or the other. I believe in learning in public, That is how I make friends and in-turn learn from them and their experiences. <br /> You can check what I'm building <a className= "underline" href="https://github.com/theyashwanthsai">here</a> . </p>
     </div>
   );
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,10 @@ function Projects() {
   return(
     <div className = "p-5 flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className = "p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Projects</h1>
-      <p>I always believe in learning by building. I love building a product, experiment, whatsoever. Building is what I wanted to do since my childhood. Turn your thoughts into something real, something which exists. Projects are what i have been building from the past two years. Here are few of the best projects I built. </p>
+      <p className="p-10 text-lg max-w-xl mb-6 font-bold">
+        I always believe in learning by building. I love building - a product, experiment, what ever. 
+        Building is what I wanted to do since my childhood. Turn your thoughts into something real, something which exists. 
+        Projects are what I have been building from the past two years. Here are few of the best projects I built. </p>
       <ul className="p-3">hi</ul>
       <ul className="p-3">hi</ul>
       <ul className="p-3">hi</ul>
