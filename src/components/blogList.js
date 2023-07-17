4b4f2c7c33c4afc4764dcaa55e95fec340a3b866
Sai Yashwanth <taddishetty34@gmail.com>
2023-07-17 09:02:45 +0530

    added blog links

--- src/components/blogList.js
+++ src/components/blogList.js
@@ -6,6 +6,17 @@ const blog = [{
       
 },
 {
+    title: 'What are Middlewares? A Comprehensive Guide for Programmers to Level Up Their Application Development',
+    link: 'https://theyashwanthsai.hashnode.dev/what-are-middlewares-a-comprehensive-guide-for-programmers-to-level-up-their-application-development',
+    tags: "Nodejs, Expressjs, Backend"
+},
+
+{
+    title: 'Mastering Backend Development: The Ultimate Guide to Building a Nodejs Blog App Server. Part - 2',
+    link: 'https://theyashwanthsai.hashnode.dev/mastering-backend-development-the-ultimate-guide-to-building-a-nodejs-blog-app-server-part-2',
+    tags: "Nodejs, Expressjs, Backend"
+},
+{
     
     title: 'Vim 101',
     link: 'https://theyashwanthsai.hashnode.dev/vim-101',
