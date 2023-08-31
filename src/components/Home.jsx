96710cc8d2831ea4b8d3d4647675a5a1a0e8a03d
Sai Yashwanth <taddishetty34@gmail.com>
2023-08-31 14:45:51 +0530

    typewriter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,45 @@
-import React from "react"
+import React, { useEffect, useRef } from "react";
 import { SocialIcon } from 'react-social-icons';
+import * as THREE from "three";
+import myFontJSON from "./myFontJSON.json";
+import Typewriter from 'typewriter-effect';
+
 
 
 function Home() {
+  
+
+
+
+
+
+
+
   return(
     <div className="p-5 flex items-center justify-center flex-col text-left pt-20 pb-6">
-      <h1 className="p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Sai Yashwanth</h1>
+      {/* <h1 className="p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Sai Yashwanth</h1>
+       */}
+<div className="p-5 flex items-center justify-center flex-col text-left pt-20 pb-6">
+<div className="p-5 text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+<Typewriter 
+  onInit={(typewriter) => {
+    typewriter.typeString('Sai Yashwanth!')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .deleteAll()
+      .callFunction(() => {
+        console.log('All strings were deleted');
+      })
+      .start();
+  }}
+/>
+</div>
+</div>
+      
       <img className = "h-100 w-96 object-cover rounded-3xl" src="./img.jpeg" alt="a cool pic" />
+      {/* <div ref={containerRef} className=""></div> */}
       <p className="p-10 text-lg max-w-xl mb-6 font-bold ">
         Hello, I'm Sai Yashwanth. I am a passionate developer, builder and learner. Welcome to my <a className= "underline" href = "https://www.dschapman.com/notes/5e6ba56d-11d3-4db0-a114-c8a415fd0bf6">Digital Garden!</a>
         <br />
