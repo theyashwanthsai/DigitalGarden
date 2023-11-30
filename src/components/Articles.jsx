14f31c5138a9bc0a9aa1ea07a7a64da32eb1979f
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-30 12:12:20 +0530

    changed few stuff

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -10,7 +10,7 @@ function Articles() {
       </div>
       <div className="p-2 text-xl  mb-6 font-semibold md:w-6/12">
       <blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
-        <p class="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">"You can make anything by writing." - C.S. Lewis</p>
+        <p class="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">"If you don’t produce, you won’t thrive—no matter how skilled or talented you are." - Cal Newport</p>
       </blockquote>
       </div>
       
