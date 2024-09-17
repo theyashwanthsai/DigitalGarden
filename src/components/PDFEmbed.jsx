cd409150daf28027b6c7a9bbe044fb6cc03c9e50
theyashwanthsai <taddishetty34@gmail.com>
2024-09-17 21:36:34 +0530

    padding

--- src/components/PDFEmbed.jsx
+++ src/components/PDFEmbed.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const PDFEmbed = () => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div className="w-full h-screen flex justify-center items-center bg-gray-100">
       <iframe 
         src="/static/resume.pdf" 
-        width="100%" 
-        height="100%" 
-        style={{ border: 'none' }}
+        className="w-1/2 h-[80%] border-none shadow-lg"
       >
         This browser does not support PDFs. Please download the PDF to view it.
       </iframe>
