16c98e0774344e912363accc8c8d5f444ca6df90
theyashwanthsai <taddishetty34@gmail.com>
2024-09-17 21:43:03 +0530

    changes

--- src/components/PDFEmbed.jsx
+++ src/components/PDFEmbed.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const PDFEmbed = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-100">
-      <iframe 
-        src="/static/resume.pdf" 
-        className="w-1/2 h-[80%] border-none shadow-lg"
-      >
-        This browser does not support PDFs. Please download the PDF to view it.
-      </iframe>
+    <div className="w-full min-h-screen flex justify-center items-center bg-gray-100 p-4">
+      <div className="w-full max-w-3xl">
+        <iframe 
+          src="/static/resume.pdf" 
+          className="w-full h-[80vh] border-none shadow-lg"
+        >
+          This browser does not support PDFs. Please download the PDF to view it.
+        </iframe>
+      </div>
     </div>
   );
 };
