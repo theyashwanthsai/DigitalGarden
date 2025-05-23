670fa20db93e3cffa64e506b4e566a3d4b3c344e
theyashwanthsai <taddishetty34@gmail.com>
2025-05-23 18:08:48 +0530

    Added a color picker

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,20 @@ import ThemeSwitcher from './ThemeSwitcher';
 function Header() {
   return(
     <div className="header-container">
+        <div className="theme-switcher-container">
+          <ThemeSwitcher />
+        </div>
       <header className="header">
         <nav>
+
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/projects" className="nav-link">Projects</Link>
           <Link to="/articles" className="nav-link">Essays</Link>
           <Link to="/about" className="nav-link">Me</Link>
         </nav>
+
         {/* Show theme switcher on all devices */}
-        <div className="theme-switcher-container">
-          <ThemeSwitcher />
-        </div>
+        
       </header>
     </div>
   );
